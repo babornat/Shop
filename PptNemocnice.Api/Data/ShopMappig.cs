580498751aacf15e3121b82a23582cc124d997a9
babornat <natya099@gmail.com>
2022-06-09 14:00:41 +0200

    Add project files.

---
+++ PptNemocnice.Api/Data/ShopMappig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Shop.Shared;
+
+namespace Shop.Api.Data;
+
+public class ShopMappig:Profile
+{
+
+    public ShopMappig()
+    {
+        CreateMap<Vybaveni, VybaveniModel>().ReverseMap();
+        CreateMap<Vybaveni, VybaveniSRevizemaModel>();
+        CreateMap<Revize, RevizeModel>().ReverseMap();
+    }
+
+}
